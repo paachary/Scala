c62d62bd4de6fec288617d6ea1084d42aebd218b
paachary <prashant_acharya74@yahoo.com>
2019-09-07 19:44:27 +0530

    more changes

--- Examples.scala
+++ Examples.scala
@@ -49,3 +49,35 @@ Bangalore
 Chennai
 Culcutta
 
+ val res = formatArgs(Array("Bangalore","Chennai","Calcutta"))
+ assert(res == "Bangalore\nChennai\nCalcutta")
+
+
+ def processFile(args : Array[String]) =
+ {
+    if (args.length > 0 )
+    Source.fromFile(args(0)).getLines().foreach(a -> println(a.lenght))
+    else
+    Console.err.println("Please enter a file name")
+ }
+ 
+ 
+import scala.io.Source
+
+def processFilenPrint(args : Array[String]) = {
+    def widthOfLength(s: String) = s.length.toString.length
+    if (args.length > 0) {
+        val lines = Source.fromFile(args(0)).getLines().toList
+        val longestLine = lines.reduceLeft(
+        (a, b) => if (a.length > b.length) a else b
+        )
+        val maxWidth = widthOfLength(longestLine)
+        for (line <- lines) {
+            val numSpaces = maxWidth - widthOfLength(line)
+            val padding = " " * numSpaces
+            println(padding + line.length + " | " + line)
+        }
+    }
+    else
+        Console.err.println("Please enter filename")
+}
