9d155279989a2a1b690d4600e162e2b7b73cf184
paachary <prashant_acharya74@yahoo.com>
2019-09-09 21:31:16 +0530

    adding class example

--- Comparison-Java-n-Scala.scala
+++ Comparison-Java-n-Scala.scala
@@ -21,4 +21,51 @@ class JavaComparisonWithScala {
 val greetStrings = Array("hi","tata","bye bye")
 newgreetStrings.foreach(println)
 
---------------------------------------------------------
+
+
+// Java program to declare a class with two variables with a constructor to initialize the variables:
+
+class JavaClass {
+
+    private int index;
+    private String name;
+
+    public JavaClass(int index, String name) {
+        this.index = index;
+        this.name = name;
+    }
+
+    public void printString() {
+        System.out.println("index = "+ this.index +": name = "+ this.name);
+    }
+}
+
+
+class MainJavaClass {
+    public static void main(String[] args) {
+
+        JavaClass javaClass = new JavaClass(2,"In Java");
+        javaClass.printString();
+    }
+
+}
+~ 
+
+// Scala program to declare a class with two variables with a constructor to initialize the variables:
+
+class ScalaClass (index :Int, name: String) {
+    def printString() = println("index = "+ index +": name = "+ name)
+}
+
+object MainScalaClass {
+    
+    def main(args : Array[String]) = {
+        
+        scalaClass = new ScalaClass(2,"In Scala")
+        
+        scalaClass.printString()
+    }
+}
+
+
+
