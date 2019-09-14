7d6b08b8dc9b96ae145dd846d8a4b3ebb6aae27e
paacharya <prashant_acharya74@yahoo.com>
2019-09-14 10:42:09 +0530

    added cat utility

--- Utilities.scala
+++ Utilities.scala
@@ -1,9 +1,10 @@
 import java.io.File
+import scala.io.Source
 import scala.io.Source._
 import java.lang.NullPointerException
 
 object Utilities extends App{
-    
+       
     def filePattern (file: File) = fromFile(file).getLines().toList
     
     def getFiles(path: String = System.getProperty("user.dir")) = (new File(path)).listFiles
@@ -35,4 +36,24 @@ object Utilities extends App{
     } yield { 
         s"${file} : ${trimmedLine}"
     }
+      
+    def cat = {
+        val width = args(0).toInt
+        
+        for (file <- args.drop(1))
+            processFile(fileName = file, width = width)
+    }
+
+    private def processFile( fileName: String, width: Int) = {
+        
+        for (line <- Source.fromFile(fileName).getLines()){
+            processLine(fileName = fileName, line = line, width = width)
+        }  
+    }    
+    
+    private def processLine( fileName:String, line:String, width:Int) = 
+       if (line.length > width) println(s"${fileName} : ${line}")    
+    
+    cat
+    
 }
