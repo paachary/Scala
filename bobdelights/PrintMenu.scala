3235bf343c66347b231dc3fc6e6db1ff58453a21
paachary <prashant_acharya74@yahoo.com>
2019-09-29 19:04:15 +0530

    new changes

--- bobdelights/PrintMenu.scala
+++ bobdelights/PrintMenu.scala
@@ -3,8 +3,10 @@ package printmenu
 import bobdelights.Fruits
 import bobdelights.bobdelights.showFruit
 object PrintMenu{
-    def main(args: Array[String]) : Unit = {
+    def main(args: Array[String]) : Unit = printMenu
+
+    def printMenu : Unit = {
         for (fruit <- Fruits.menu)
-              showFruit(fruit)
+        showFruit(fruit)
     }
 }
