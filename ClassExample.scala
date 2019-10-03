e8d0f22fc0b394a618733fcb3eac5a32372c56e6
paachary <prashant_acharya74@yahoo.com>
2019-10-03 10:57:53 +0530

    more examples

--- ClassExample.scala
+++ ClassExample.scala
@@ -18,14 +18,159 @@ class Time {
     def minute_=  
         (x: Int) = { m = x }
     
-    }
+}
 
 
-    class Thermometer {
-        var celsius: Float = _
-        def fahrenheit = celsius * 9 / 5 + 32
-        def fahrenheit_= (f: Float) = {
-        celsius = (f - 32) * 5 / 9
-        }
-        override def toString = s"${fahrenheit} F/ ${celsius} C"
+class Thermometer {
+    var celsius: Float = _
+    def fahrenheit = celsius * 9 / 5 + 32
+    def fahrenheit_= (f: Float) = {
+    celsius = (f - 32) * 5 / 9
     }
+    override def toString = s"${fahrenheit} F/ ${celsius} C"
+}
+
+trait Queue[+A] {
+    def head : A
+
+    def tail: Queue[A]
+
+    def enqueue[U >: A] (x:U): Queue[U]
+}
+object  Queue { 
+    def apply[A](xs: A*): Queue[A] = new QueueImpl[A](xs.toList, Nil)
+
+    private class QueueImpl[+A]  ( 
+        private val leading: List[A],
+        private val trailing: List[A]
+    ) extends Queue[A]{
+
+        override def toString(): String = leading:::trailing mkString " "
+
+        def mirror = if (leading.isEmpty) 
+                        new QueueImpl(trailing.reverse, Nil) 
+                    else 
+                        this
+
+        def head = mirror.leading.head 
+        
+        def tail :QueueImpl[A] = {
+            val q = mirror
+            new QueueImpl(q.leading.tail, q.trailing)
+        } 
+        
+        def enqueue[U >: A] (x:U) = new QueueImpl(leading, x::trailing)
+    } 
+}
+
+class Publication(val title: String)
+
+class Book(title: String) extends Publication(title)
+
+object Library {
+
+    val books : Set[Book] = 
+    Set(
+        new Book("Programming in Scala"),
+        new Book("Prashant's book")
+    )
+
+    def printBookList(info: Book => AnyRef) = {
+        for (book <- books) println(info(book))
+    }
+}
+
+object Customer extends App {
+    def getTitle(p : Publication) : String = p.title
+
+    Library.printBookList(getTitle)
+}
+
+abstract class CurrencyZone {
+    
+    type Currency <: AbstractCurrency
+
+    def make(x: Long) : Currency
+
+    abstract class AbstractCurrency {
+        val amount : Long
+        def designation : String
+
+        def + (that: Currency) : Currency = 
+            make(this.amount + that.amount)
+
+        def - (that: Currency) : Currency = 
+            make(this.amount - that.amount)
+
+        def / (that: Currency) : Currency = 
+            make(this.amount / that.amount)
+
+        def / (that: Double) : Currency = 
+            make((this.amount / that).toLong)
+
+        def * (that: Currency) : Currency = 
+            make(this.amount * that.amount)
+
+        def * (that: Double) : Currency = 
+            make((this.amount * that).toLong)
+
+        def from(other: CurrencyZone#AbstractCurrency) : Currency =
+          make(math.round(other.amount.toDouble *  
+            Converter.exchangeRate(other.designation)(this.designation)))
+
+        def decimals(n:Long) : Int = 
+            if (n == 1) 0 else 1 + decimals(n/10)
+
+        override def toString(): String = ((amount.toDouble / CurrencyUnit.amount.toDouble)
+            formatted("%." + decimals(CurrencyUnit.amount)+"f") + " " + designation)
+    }
+    val CurrencyUnit : Currency;
+}
+
+object Converter{
+    var exchangeRate = Map (
+        "USD" -> Map("USD" -> 1.0, "EUR" -> 0.91,
+                     "INR" -> 71.2),
+        "EUR" -> Map("USD" -> 1.10, "EUR" -> 1.0,
+                     "INR" -> 77.99),
+        "INR" -> Map("USD" -> 0.0014, "EUR" -> 0.013,
+                     "INR" -> 1.0)        
+    )
+}
+
+object US extends CurrencyZone {
+    abstract class Dollar extends AbstractCurrency{
+        def designation: String = "USD"
+    }
+    type Currency = Dollar
+    def make(cents: Long): Dollar = new Dollar {val amount: Long = cents}
+
+    val Cent = make(1)
+    val Dollar = make(100)
+    val CurrencyUnit: Dollar = Dollar
+}
+
+object Europe extends CurrencyZone {
+    abstract class Euro extends AbstractCurrency{
+        def designation: String = "EUR"
+    }
+    type Currency = Euro
+    def make(cents: Long): Euro = new Euro {val amount: Long = cents}
+
+    val Cent = make(1)
+    val Euro = make(100)
+    val CurrencyUnit : Euro = Euro
+}
+
+object India extends CurrencyZone {
+    abstract class Rupee extends AbstractCurrency{
+        def designation: String = "INR"
+    }
+
+    type Currency = Rupee
+    def make(paisa: Long): Rupee = new Rupee {val amount: Long = paisa}
+
+    val Paisa = make(1)
+    val Rupee = make(100)
+    val CurrencyUnit: Rupee = Rupee
+}
